b73964dc6d2a61bc0c4d194e7da12087e2a5a321
alyannasantos <>
2021-05-20 02:16:12 +0000

    Green stage changes

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,9 +3,34 @@ package recursionAssignment;
 public class Recursion{
 
   public static int count7(int n) {
-  
-    return 0;
 
+    if (n < 0) {
+      return -1;
+    }   
+
+    String strNum = n + "";
+    int maxIndex = strNum.length() - 1;
+    return checkDigit(strNum, 0, maxIndex);
+    
+  }
+
+  private static int checkDigit(String strNum, int curIndex, int maxIndex) {
+
+    if (curIndex > maxIndex) {
+      return 0;
+    }
+
+    int counter = 0;
+
+    if (strNum.charAt(curIndex) == '7') {
+      counter++;
+    }
+
+    curIndex++;
+
+    counter = counter + checkDigit(strNum, curIndex, maxIndex);
+    return counter;
 
   }
+
 }
